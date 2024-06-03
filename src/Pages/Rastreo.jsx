460e4350c2c18e87dfb37d3968d82e8c8e774726
Mario R Viñas Luciano <105821899+MarioRVinasLuciano@users.noreply.github.com>
2024-06-03 16:56:49 -0400

    rastreo, info arreglada

--- src/Pages/Rastreo.jsx
+++ src/Pages/Rastreo.jsx
@@ -202,7 +202,7 @@ export default function Tracking() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
               </svg>
             </div>
-            <div className='hidden md:block lg:block'>
+            <div className='hidden sm:block md:block lg:block'>
               <p className="text-xs sm:text-sm md:text-normal text-black">
                 Rastree su carga ingresando su número de contenedor, conocimiento de embarque,
                 orden o referencia para obtener información de seguimiento de carga instantánea
